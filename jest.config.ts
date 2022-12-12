c69c4e8b15f5f6931b9280b1bfb5068e7b125f02
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-12 21:29:24 +0900

    テスト砂場環境卍

--- jest.config.ts
+++ jest.config.ts
@@ -76,16 +76,7 @@ export default {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: ["js", "ts", "json", "vue"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -175,7 +166,8 @@ export default {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
   transform: {
-    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.vue$": "vue-jest",
   },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
